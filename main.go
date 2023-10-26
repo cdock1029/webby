7fae39787ce99f929ea1f4932ba6939c8748ea9e
Conor Dockry <conordockry@gmail.com>
2023-10-25 22:21:05 -0400

    fragments WIP

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"embed"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	webbyDb "github.com/cdock1029/webby/db"
 
 	"database/sql"
 
-	_ "github.com/lib/pq"
+	"github.com/lib/pq"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -60,7 +62,7 @@ func defineAppRoutes(r *chi.Mux) {
 			return
 		}
 
-		t.ExecuteTemplate(w, "index.html.tmpl", properties)
+		t.ExecuteTemplate(w, "index.html.tmpl", map[string]any{"Properties": properties})
 	})
 	r.Get("/properties", func(w http.ResponseWriter, r *http.Request) {
 
@@ -69,8 +71,7 @@ func defineAppRoutes(r *chi.Mux) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-
-		t.ExecuteTemplate(w, "_fragment.properties.html.tmpl", properties)
+		t.ExecuteTemplate(w, "_fragment.properties.html.tmpl", map[string]any{"Properties": properties})
 	})
 	r.Post("/", func(w http.ResponseWriter, r *http.Request) {
 
@@ -85,11 +86,36 @@ func defineAppRoutes(r *chi.Mux) {
 			return
 		}
 		if _, err := repo.CreateProperty(r.Context(), name); err != nil {
-			w.WriteHeader(500)
+			data := make(map[string]any)
+			if err, ok := err.(*pq.Error); ok {
+				log.Default().Println("Database error: ", err.Message)
+				log.Default().Println("Database error: ", err.Detail)
+				log.Default().Println("Database error: ", err.Hint)
+				data["Error"] = err.Detail
+			}
+			t.ExecuteTemplate(w, "_fragment.property.form.html.tmpl", data)
 			return
 		}
 		w.Header().Set("HX-Trigger", "newProperty")
 		w.WriteHeader(204)
+	})
+	r.Get("/property/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 32)
+		edit := r.URL.Query().Get("edit")
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		property, err := repo.GetProperty(r.Context(), int32(id))
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		if edit != "" {
+			t.ExecuteTemplate(w, "_fragment.property.edit.html.tmpl", property)
+		} else {
+			t.ExecuteTemplate(w, "_fragment.property.html.tmpl", property)
+		}
 	})
 	r.Delete("/property/{id}", func(w http.ResponseWriter, r *http.Request) {
 		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 32)
@@ -106,6 +132,35 @@ func defineAppRoutes(r *chi.Mux) {
 			w.WriteHeader(500)
 			return
 		}
+		w.Header().Set("HX-Trigger", fmt.Sprintf("propertyDeleted%v", id))
+		//w.WriteHeader(200)
+		w.WriteHeader(204)
+	})
+	r.Put("/property/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id, err := strconv.ParseInt(chi.URLParam(r, "id"), 10, 32)
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		if err := r.ParseForm(); err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		form := r.PostForm
+		name := strings.TrimSpace(form.Get("name"))
+		if name == "" {
+			w.WriteHeader(204)
+			return
+		}
+		property, err := repo.UpdateProperty(r.Context(), webbyDb.UpdatePropertyParams{ID: int32(id), Name: name})
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("HX-Trigger", fmt.Sprintf("propertyUpdated%v", property.ID))
+		t.ExecuteTemplate(w, "_fragment.property.html.tmpl", property)
+	})
+	r.Get("/null", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 	})
 }
